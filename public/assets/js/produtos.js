3ab6002412f9a0de19039f749d2cb169177403c9
Walter RibeiroS <riibeirowalter@gmail.com>
2026-06-16 16:58:22 -0300

    paginacao a principio funcionado, falta testes

--- public/assets/js/produtos.js
+++ public/assets/js/produtos.js
@@ -124,7 +124,127 @@ async function carregarProdutosGrid(){
             criarCardProduto(produto)
         );
     });
+    renderizarPaginacao(totalPaginas)
+}
 
+/* funcoes para fazer a paginacao */
+
+function renderizarPaginacao(totalPaginas) {
+
+    const pagination = document.getElementById("pagination")
+
+    pagination.replaceChildren()
+
+    if (totalPaginas <= 0){
+        return // finaliza 
+    }
+
+    if(totalPaginas === 1){
+        const li = document.createElement("li")
+
+        li.innerHTML = `
+            <a class="active">1</a>
+        `
+
+        pagination.appendChild(li)
+
+        return
+    }
+
+    // <<
+    pagination.appendChild(
+        criarBotaoPaginacao(
+            '<i class="fa-solid fa-angles-left"></i>',
+            1,
+            paginaAtual === 1
+        )
+    );
+
+    // <
+    pagination.appendChild(
+        criarBotaoPaginacao(
+            '<i class="fa-solid fa-angle-left"></i>',
+            paginaAtual - 1,
+            paginaAtual === 1
+        )
+    );
+
+    //logica por tras das paginas mostradas
+
+    let inicio = paginaAtual - 2
+    let fim = paginaAtual + 2
+
+    if (inicio < 1){
+        inicio = 1;
+        fim = Math.min(5, totalPaginas)
+    }
+
+    if (fim > totalPaginas){
+        fim = totalPaginas
+        inicio = Math.max(1, totalPaginas - 4)
+    }
+
+    //criar numeros
+
+    for(let pagina = inicio; pagina <= fim; pagina++){
+        pagination.appendChild(
+            criarBotaoPaginacao(
+                pagina,
+                pagina,
+                false,
+                pagina === paginaAtual
+            )
+        );
+    }
+
+    // >
+    pagination.appendChild(
+        criarBotaoPaginacao(
+            '<i class="fa-solid fa-angle-right"></i>',
+            paginaAtual + 1,
+            paginaAtual === totalPaginas
+        )
+    );
+
+    // >>
+    pagination.appendChild(
+        criarBotaoPaginacao(
+            '<i class="fa-solid fa-angles-right"></i>',
+            totalPaginas,
+            paginaAtual === totalPaginas
+        )
+    );
+}
+
+function criarBotaoPaginacao(conteudo, paginaDestino, desabilitado = false, ativo = false){
+
+    const li = document.createElement("li")
+    const a = document.createElement("a")
+
+    a.innerHTML = conteudo
+
+    if (ativo){
+        a.classList.add("active")
+    }
+
+    if (desabilitado){
+        a.classList.add("desabilitado")
+    }else {
+        a.addEventListener("click", () => {
+            paginaAtual = paginaDestino
+
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+
+            carregarProdutosGrid()
+        })
+    }
+
+    li.append(a)
+
+    return li
 }
 
 carregarProdutosGrid()
